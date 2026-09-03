702ac19dc7c5a1164fa228fadc0851fe5fb7a6f9
ryuzzzzzzxzz <ryufelix45@gmail.com>
2026-09-03 12:44:03 +0700

    Feat: implement view homepage

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/homepage', [App\Http\Controllers\HomepageController::class, 'index']);
