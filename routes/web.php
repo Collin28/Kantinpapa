200e7c0947456c534336acf0a85131192304dd7b
Collin28 <collin282829@gmail.com>
2026-09-13 23:13:40 +0700

    reset

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,1 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/homepage', [App\Http\Controllers\HomepageController::class, 'index']);
-
-use App\Http\Controllers\RegisterpageController;
-
-// Route Tampil Halaman Register
-Route::get('/register', [RegisterpageController::class, 'index'])->name('register');
-
-// Route Proses Submit Register (POST)
-Route::post('/register', [RegisterpageController::class, 'store'])->name('register.store');
 
